531823d3f93d8e7303a6150f004c386abe6c643a
Stutya Mallick <stutya.mallick@gmail.com>
2020-04-26 09:39:36 +0530

    Header re structuring

--- src/app-header/index.js
+++ src/app-header/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import './index.css';
-import logo from '../logo.svg';
+import logo from '../resources/images/logo.svg';
+import logout from '../resources/images/logout.svg';
+import moment from 'moment';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div className="rowFlex">
-                <div className="flex-2 app-logoCt"><img src={logo} className="app-logo" alt="logo" /></div>
-                <div className="flex-4 app-titleCt">
-                    <label className="app-title">{this.props.applicationName}</label>
-                </div>
-                <div className="flex-2 rev-rowFlex">
-                    <div className="colFlex app-userDetCt">
-                        <div className="app-userDetText">
-                            SignedIn as Stutya Mallick</div>
-                        <div className="app-userSignoutText rev-rowFlex">SignOut</div>
+export default function Header(props) {
+    const currentLogin = moment(new Date()).format('ddd, DD/MM/YY hh:mm A');
+    return (
+        <React.Fragment>
+            <div className="app_logo_container">
+                <img src={logo} alt="logo" className="app_logo" />
+            </div>
+            <div className="app_title_container">
+                <label className="app_title">{props.applicationName}</label>
+            </div>
+            <div className="user_detail_container">
+                <div>
+                    <div className="user_detail_text">Signed in as Stutya Mallick</div>
+                    <div className="user_detail_text">Current login: {currentLogin}</div>
+                    <div className="user_detail_text">
+                        <img src={logout} alt="logout" className="app_logout" />
+                        SignOut&nbsp;&nbsp;
                     </div>
                 </div>
             </div>
-        );
-    }
+        </React.Fragment>
+    );
 }
-
-export default Header;
